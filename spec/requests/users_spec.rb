15001cf5944e9128d4553809098e485acad7bcf6
ldrohan <drohanluke@gmail.com>
2014-05-29 13:37:32 -0700

    updating test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,1 +1,26 @@
 require 'spec_helper'
+
+describe "Users response" do
+
+  describe "root path" do
+    it "should pass" do
+      get "/"
+      response.status.should be(302)
+    end
+  end
+
+  describe "Signup Users path" do
+    it "should pass" do
+      get "/users/sign_up"
+      response.status.should be(200)
+    end
+  end
+
+  describe "Signin Users path" do
+    it "should pass" do
+      get "/users/sign_up"
+      response.status.should be(200)
+    end
+  end
+
+end
