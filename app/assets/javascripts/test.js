7a3aac53359daa5abe09fcadc38301578646664f
honeycomb80 <honeycomb8080@gmail.com>
2014-05-30 10:07:47 -0700

    added things

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+	$('newreleasebox').animate({top:'+=10em'},500);
+});
+
+
+
+
 // $(document).ready(funciton(){
 // 	$('.albumbox').perfectScrollbar('update');
 // });
